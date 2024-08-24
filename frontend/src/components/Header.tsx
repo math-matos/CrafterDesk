5da9851b95a02a5945b53914bd3ac533972db859
Matheus Matos Bernardo <matheus.bernardoo1001@gmail.com>
2024-08-23 21:43:30 -0300

    style: change logo

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import logo from "../assets/logo2.png";
+import logo from "../assets/logo.png";
 
 import Button from "@mui/material/Button";
 import { Typography } from "@mui/material";
@@ -8,12 +8,12 @@ export default function Header() {
   return (
     <header className="navbar">
       <div className="logo">
-        <img src={logo} />
-        <h2>Craft Desk</h2>
+        <img src={logo} style={{width: 150, borderRadius: 20}}/>
+        {/* <h2>Craft Desk</h2> */}
       </div>
       <nav className="nav-links">
-        <a href="#features">Sobre o projeto</a>
-        <a href="#contact">Contact</a>
+        <a href="#features">About the project</a>
+        <a href="#pricing">Pricing</a>
         <Button variant="contained" href="#signup" style={{ color: "white", height: "3rem" }}>
           Get Started
         </Button>
